eb48589c6b20d45af2559ab570fd71270bd6108e
Ramsey Seweingyawma <rsewein@gmail.com>
2018-07-17 13:45:30 -0600

    Added remaining inputs.

--- src/components/card.js
+++ src/components/card.js
@@ -8,31 +8,62 @@ class Card extends Component {
         super()
 
         this.state = {
-            color: 'BLUE',
-            pluralNoun: ''
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+            nounTwo:'',
+            adjectiveThree:'',
+            celebTwo:'',
+            celebThree:'',
+            adjectiveFour:'',
+            nounThree:'',
+            celebFour:'',
+            adjectiveFive:''
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     handleInputChange (event) {
-        this.setState({ color: event.target.value })
+        this.setState({ [event.target.name]: event.target.value })
+        console.log(this.state);
     }
 
     render () {
 
-        return (
-            <div className="card">
-                <h1>{this.state.color}</h1>
-                <input value={this.state.color} onChange={(event) => this.handleInputChange(event)} />
-            </div>
-        )
+        const inputData = [
+            {title: 'Color', state: this.state.color, name: 'color'},
+            {title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
+            {title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne'},
+            {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
+
+            {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
+            {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
+            {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
+            {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
+            
+            {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
+            {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
+            {title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
+            {title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
+            
+            {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
+            {title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
+            {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
+            {title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive'},
+        ]
 
         return (
             <div className="card">
-                <h1>{this.state.color}</h1>
-                { Input('Color') }
-                { Input('Plural Noun') }
+                {
+                    inputData.map(data => Input( (data), this.handleInputChange) )
+                }
+
             </div>
         )
     }
