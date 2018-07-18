9a82604d3a687827a0b9909c34bba17b06c3ad17
Ramsey Seweingyawma <rsewein@gmail.com>
2018-07-18 11:20:16 -0600

    Put number on labels on inputs

--- src/components/card.js
+++ src/components/card.js
@@ -76,7 +76,9 @@ class Card extends Component {
             <form onSubmit={this.handleFormSubmit} className="card">
                 <div className="card_inputs">
                 {
-                    inputData.map(data => Input( (data), this.handleInputChange) )
+                    inputData.map((data, index) => {
+                        return Input( (data), this.handleInputChange, index)
+                    })
                 }
                 </div>
                 <button type="submit">{!this.state.contentVisible ? 'Generate Madlib' : 'Clear Form'}</button>
